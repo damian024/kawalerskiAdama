dcb70819f81b1eeb8f2e45223a2e4dbf8aaadb4c
Damian Zoladek <damian.zolad@gmail.com>
2024-06-20 22:49:50 +0200

    added questions and penalties

--- common/configProvider.ts
+++ common/configProvider.ts
@@ -2,119 +2,432 @@ export const Config: GameConfig= {
     questions: [
         {
             id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time: 10,
+            text : "Jaki jest najbardziej bolesny obowiązek domowy Karo?",
+            time: 60,
             validAnswerIndex: 2,
             answers: [
-                "Odpowiedź 1",
-                "Odpowiedź 2",
-                "Odpowiedź 3",
-            ]
+                "Wynoszenie śmieci",
+                "Gotowanie",
+                "Sprzątanie łazienki",
+              ]
         },
         {
             id: 1,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time: 10,
+            text : "O zrobieniu czego w przyszłości marzy Karolina:",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "Tatuaż",
+                "Kolczyk w pępku",
+                "Powiększenie ust",
+              ]
+        },
+        {
+            id: 2,
+            text : "Co z tych 3 dań lubi najbardziej",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "zupa owocowa",
+                "Galareta z warzywami",
+                "Duszona marchewka",
+              ]
+        },
+        {
+            id: 3,
+            text : "Z podanych 3 jak Karo najbardziej lubi być przez Ciebie nazywana: ",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Kochanie",
+                "Skarbie",
+                "Misiu",
+              ]
+        },
+        {
+            id: 4,
+            text : "Co dla Karoliny jest najbardziej w Tobie irytujące?",
+            time: 60,
             validAnswerIndex: 2,
             answers: [
-                "Odpowiedź 2",
-                "Odpowiedź 3",
-                "Odpowiedź 4",
+                "Nienakładanie papieru na rolkę, gdy się skończy",
+                "Rzucenie spodni gdzie popadnie",
+                "„Zagram tylko jedną gierkę w cska” ",
+              ]
+        },
+        {
+            id: 5,
+            text : "Jaką ma grupę krwi?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "0+",
+                "B-",
+                "Sama nie wiem",
             ]
         },
+        {
+            id: 6,
+            text : "Jaki film jest tak dobry, że chciałaby jeszcze raz zobaczyć go po raz “pierwszy”?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Chłopiec w pasiatej piżamie",
+                "Spirited away: W kranie Bogów",
+                "Mój przyjaciel Hachiko",
+              ]
+        },
+        {
+            id: 7,
+            text : "Cecha którą najbardziej w Tobie ceni to:",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Szczerość",
+                "Wsparcie",
+                "Rodzinność",
+              ]
+        },
+        {
+            id: 8,
+            text : "Wybierz prawdziwy numer telefonu Karo:",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "518671496",
+                "517671396",
+                "518671596",
+            ]
+        },
+        {
+            id: 9,
+            text : "Jakie inne imię było alternatywą dla pieska",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Zosia",
+                "Gaja",
+                "Tylko i wyłącznie Zoya",
+            ]
+        },
+        {
+            id: 10,
+            text : "Ile razy zapomniałeś wyjść z Zoyką na spacer XD:",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "9",
+                "14",
+                "6",
+              ]
+        },
+        {
+            id: 11,
+            text : "Ile razy zapomniałeś wyjść z Zoyką na spacer XD:",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "9",
+                "14",
+                "6",
+              ]
+        },
+        {
+            id: 12,
+            text : "Jaka jest najdziwniejsza rzecz, którą kupiła? ",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Drewniany stojak do telefonu",
+                "Bańki odchudzające",
+                "Powiększacz do ust",
+              ]
+        },
+        {
+            id: 13,
+            text : "Jaka była ulubiona zabawka Karo w dzieciństwie?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Zielono niebieski traktor",
+                "Lalka wielkości małej Karolinki",
+                "Barbie łyżwiarka",
+            ]        
+        },
+        {
+            id: 14,
+            text : "Kim chciałaby być jak była dzieckiem?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Aktorką",
+                "Piosenkarką",
+                "Nauczycielem matematyki",
+            ]        
+        },
+        {
+            id: 14,
+            text : "Kim chciałaby być jak była dzieckiem?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Aktorką",
+                "Piosenkarką",
+                "Nauczycielem matematyki",
+            ]        
+        },
+        {
+            id: 15,
+            text : "Ile glo dziennie paliła w szczytowym momencie?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "25",
+                "15",
+                "20",
+            ]        
+        },
+        {
+            id: 16,
+            text : "Gdyby miała wziąć tylko 3 rzeczy na bezwzludną wyspę, to zabrałaby?:",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Adasia, książkę, telefon",
+                "Adasia, chipsy, telefon",
+                "Adasia, żelki, telefon",
+            ]        
+        },
+        {
+            id: 17,
+            text : "Jakie jest jej ulubione wspomnienie z adasiem?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Pierwsze spotkanie",
+                "Zaręczyny",
+                "Nasz pierwszy wspólny wyjazd",
+            ]        
+        },
+        {
+            id: 18,
+            text : "Ile dzieci chciałaby mieć Karo?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "1",
+                "2",
+                "3",
+            ]        
+        },
+        {
+            id: 19,
+            text : "Jaki kolor włosów miała Karo, gdy się urodziła?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Była łysa",
+                "Ciemny brąz",
+                "Jasny blond",
+            ]        
+        },
+        {
+            id: 20,
+            text : "Imię zwierzątka z dzieciństwa Karo?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Milka",
+                "Mila",
+                "Mika",
+            ]        
+        },
+        {
+            id: 21,
+            text : "Na jakieś pozycji grała Karo w siatkówkę?",
+            time: 60,
+            validAnswerIndex: 2,
+            answers: [
+                "Rozgrywająca, ale lubiąca bronić",
+                "Środkowa, ale nie lubiąca serwować",
+                "Libero, ale uwielbiająca rozgrywać",
+            ]        
+        },
+        {
+            id: 22,
+            text : "Jakie jest jej imię z bierzmowania?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Magdalena",
+                "Elżbieta",
+                "Weronika",
+            ]        
+        },
+        {
+            id: 23,
+            text : "Do jakiego fast-fooda porównałaby Adasia?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "BigMac",
+                "ChickenBox",
+                "Cheeseburger",
+            ]        
+        },
+        {
+            id: 24,
+            text : "Co by zrobiła, gdyby wygrała na loterii?",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "Spłaciłaby długi i zainwestowała resztę",
+                "Kupiłaby dom i zainwestowała resztę",
+                "Pomogłaby rodzinie i zainwestowała resztę",
+            ]        
+        },
+        {
+            id: 26,
+            text : "Do jakiego zwierzęcia porównałaby Adasia?",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "Lew",
+                "Tygrys",
+                "Puma",
+            ]
+        },
+        {
+            id: 27,
+            text : "Jakie 3 słowa najlepiej opisują Adasia",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                "Trochę zabawny",
+                "Czasami zwariowany",
+                "Przeważnie zdecydowany",
+            ]
+        },
+        {
+            id: 28,
+            text : "Liczba mieszkańców Białej Podlaskiej",
+            time: 60,
+            validAnswerIndex: 0,
+            answers: [
+                " 53 848",
+                "56 742",
+                "59 346",
+            ]
+        },
+        {
+            id: 29,
+            text : "Powierzchnia Białej Podlaskiej",
+            time: 60,
+            validAnswerIndex: 1,
+            answers: [
+                "51,2 km2",
+                "49,5 km2",
+                "53,7 km2",
+            ]
+        }
     ],
     penalties: [
         {
             id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 5,
+            text : "Liczysz do 10 i musisz kogoś złapać - złapana osoba wykonuje kolejne zadanie z Tobą",
+            time : 60        
         },
         {
             id: 1,
-            text : "Kara 2",
-            time : 60,
+            text : "Przetestujemy Twoje pewne ręce - musisz ułożyć 3 stopniowy domek z kart",
+            time : 60        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 2,
+            text : "Zobaczmy jak sobie radzisz z origami - dostaniesz od prowadzących kartkę papieru, robisz z niej samolot, który musisz rzucić tak aby trafił w wyznaczone miejsce",
+            time : 120        
         },
         {
-            id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 60,
+            id: 3,
+            text : "Ktoś z widowni poda Ci 2 cytryny - weź je , wycisnij do szklaneczki, po czym wyzeruj bez krzywienia się (krzywisz się - przegrywasz)",
+            time : 30        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 4,
+            text : "Zobaczmy jak poradzisz sobie z orientacją w terenie - osoba z widowni założy  Ci opaskę na oczy, idź z nią po piwko dla świadka",
+            time : 120        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 5,
+            text : "Weź telefon i zadzwoń przez FaceTime do Karo - bez używania słów pokaż Jej tak żeby zrozumiała hasło “nie jestem pijany” - (nie odbierze - przegrywasz)",
+            time : 90        
         },
         {
-            id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 60,
+            id: 6,
+            text : "Uwaga, za chwilę zostanie Ci podany napój, poproś o słomkę i wypij zawartość",
+            time : 15        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 7,
+            text : "Adaś skup się! Leć do kuchni, weź  jajko z lodówki, wróć do nas połóż je na łyżeczkę i zrob rundkę wokół domu",
+            time : 120        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 8,
+            text : "Uwaga - leć do kuchni po łyżeczkę i używając niej zerujesz drinka",
+            time : 90        
         },
         {
-            id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 60,
+            id: 10,
+            text : "Wykonaj bieg pijaka - ktoś z widowni obraca Cię kilka razy i musisz przebiec do znacznika, ominąć go i wrócić",
+            time : 30        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 11,
+            text : "W końcu coś przyjemnego, chwila wytchnienia - wypij piwko z leja",
+            time : 90        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 12,
+            text : "Widać że jesteś trochę niedogrzany - 20 pompek",
+            time : 30        
         },
         {
-            id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 60,
+            id: 13,
+            text : "Sprawdzimy Twojego cela - udaj się z jednym z prowadzących, wytłumaczą Ci co robić",
+            time : 120        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 14,
+            text : "W końcu coś przyjemnego, chwila wytchnienia - wypij piwko z leja",
+            time : 90        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 15,
+            text : "Może trochę zgłodniałeś- zjedz to, co Ci przygotowali prowadzący",
+            time : 90        
         },
         {
-            id: 0,
-            text : "Musisz wziąc z kosza na rekwizyty pistolet na wodę, napełnij go. Gdy będziesz gotowy kliknij start - twoim celem jest ustrzelenie minimum 5 osób",
-            time : 60,
+            id: 16,
+            text : "W kuchni są piwka - przynieś każdemu po jednym!",
+            time : 180        
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 17,
+            text : "Odpoczynek - wybierz jedną osobę",
+            time : 15
         },
         {
-            id: 1,
-            text : "Kara 2",
-            time : 60,
+            id: 18,
+            text : "Wybierz 3 ochotników - następnie stań w wyznaczonym Ci miejscu - każdy z nich ma wytyczoną linie, po 5 balonów z wodą  i rzucają na zmianę w niego, a on unika",
+            time : 180
+        },
+        {
+            id: 19,
+            text : "Wybierz jedną osobę, zostaje kapitanem przeciwnej drużyny i gramy we flanki",
+            time : 600,
         }
-
+    
     ]
 }
 
