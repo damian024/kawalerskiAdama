b09342688dbb465960a283ed54db06e6bb6e8310
Damian Zoladek <damian.zolad@gmail.com>
2024-06-21 13:08:20 +0200

    added missing key on results page

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -4,9 +4,10 @@ export default function ResultsPage(){
     return <div className="flex text-xl flex-col gap-2">
         {
             Config.questions.map((x, i) =>
-                <div className="flex flex-row gap-3 border-b-2">
-                    <span>{i + 1}.</span>
-                    <span className="text-green-700">{String.fromCharCode(65 + x.validAnswerIndex)}</span>
+                <div key={i} 
+                     className="flex flex-row gap-3 border-b-2">
+                        <span>{i + 1}.</span>
+                        <span className="text-green-700">{String.fromCharCode(65 + x.validAnswerIndex)}</span>
                 </div>
             )
         }   
