b89d33767b77a9f539bbfdad8769fca7a3807cdf
Damian Zoladek <damian.zolad@gmail.com>
2024-06-21 13:04:26 +0200

    added result page for operator

---
+++ app/results/page.tsx
@@ -0,0 +1,14 @@
+import { Config } from "@/common/configProvider"
+
+export default function ResultsPage(){
+    return <div className="flex text-xl flex-col gap-2">
+        {
+            Config.questions.map((x, i) =>
+                <div className="flex flex-row gap-3 border-b-2">
+                    <span>{i + 1}.</span>
+                    <span className="text-green-700">{String.fromCharCode(65 + x.validAnswerIndex)}</span>
+                </div>
+            )
+        }   
+    </div>
+} 
